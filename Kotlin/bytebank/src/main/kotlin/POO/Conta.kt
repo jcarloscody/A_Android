1aa649e7016e3f7b8c31117811072024b77f17cc
Josué <josuecarlosos2@gmail.com>
2023-04-12 00:25:10 -0300

    properties

--- Kotlin/bytebank/src/main/kotlin/POO/Conta.kt
+++ Kotlin/bytebank/src/main/kotlin/POO/Conta.kt
@@ -2,6 +2,21 @@ package POO
 
 class Conta {
 
+    // properties - set e get. as variaveis já tem, nao precisa implementar uma funcao para isto
+    //por defualt já vem
+
+        private var titular=""
+            private set(value) {}
+
+
+    private  var conta: Int = 0
+        set(value) {
+            if (value>10){
+                field = 0
+            }
+        }
+        get() = field
+
     //está retornando Unit
     fun deposita(valor:Double):Unit{
 
@@ -10,4 +25,5 @@ class Conta {
     fun trans():Boolean{
         return  true
     }
+
 }
